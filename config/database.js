95fe340bd600ab094abec347da4d2e51e812c949
mdmonir027 <mmislam027@gmail.com>
2021-11-20 15:20:46 +0600

    change database locahost to dynamichost

--- config/database.js
+++ config/database.js
@@ -1,11 +1,12 @@
 const { Sequelize } = require('sequelize');
+const hostname = process.env.DATABASE_HOST_NAME;
 const database = process.env.DATABASE_NAME;
 const username = process.env.DATABASE_USERNAME;
 const password = process.env.DATABASE_PASSWORD;
 
 // Option 3: Passing parameters separately (other dialects)
 const db = new Sequelize(database, username, password, {
-  host: 'localhost',
+  host: hostname,
   dialect: 'mysql',
 
   pool: {
